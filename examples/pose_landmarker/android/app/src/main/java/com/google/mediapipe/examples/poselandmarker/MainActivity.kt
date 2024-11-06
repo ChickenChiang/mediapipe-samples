af7fdeb6e3b04f22cde636aa792028bd69d40a08
ChickenChiang <potatoeschiang@gmail.com>
2024-11-06 14:13:58 +0800

    Update methods to have the pushUp logic to stop counting when the timer is not running

--- examples/pose_landmarker/android/app/src/main/java/com/google/mediapipe/examples/poselandmarker/MainActivity.kt
+++ examples/pose_landmarker/android/app/src/main/java/com/google/mediapipe/examples/poselandmarker/MainActivity.kt
@@ -22,21 +22,15 @@ import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import com.google.mediapipe.examples.poselandmarker.databinding.ActivityMainBinding
 import kotlin.math.floor
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
-
-
     private lateinit var activityMainBinding: ActivityMainBinding
-//    private val viewModel: MainViewModel by viewModels()
-
-
-
     val viewModel: MainViewModel by viewModels()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,48 +39,45 @@ class MainActivity : AppCompatActivity() {
 
         var startButton = findViewById<FloatingActionButton>(R.id.startButton)
         var resetButton = findViewById<FloatingActionButton>(R.id.resetButton)
-        startButton.setOnClickListener { view ->
-            viewModel.startStopTimer()
-            Log.d(TAG, "Start/Stop button pressed")
-        }
         viewModel.count.observe(this) { count ->
             updatePushUpCounter(count)
         }
         viewModel.secondsRemaing.observe(this) { secondsLeft ->
             updateTimerUI(secondsLeft)
         }
+        startButton.setOnClickListener { view ->
+            viewModel.startStopTimer()
+            Log.d(TAG, "Start/Stop button pressed")
+        }
         resetButton.setOnClickListener { view ->
             viewModel.resetTimer()
+            Snackbar.make(
+                view,
+                "Counter has been reset",
+                Snackbar.LENGTH_SHORT
+            ).show()
         }
     }
 
-    fun updateTimerUI(secondsLeft : Long) {
+    fun updateTimerUI(secondsLeft: Long) {
+        // Calculate how many minutes left. This is more for future proofing if we want to take more than a minute
         var minutesLeftString = floor((secondsLeft / 60).toDouble()).toInt().toString()
+        // Get the remaining seconds
         var secondsLeftString = (secondsLeft % 60).toString()
+        // Add a zero infront if secondsleft are in the single digits
         if (secondsLeftString.length < 2) {
             secondsLeftString = "0" + secondsLeftString
         }
+
+        //Update the UI
         var timerText = findViewById<TextView>(R.id.countdown_Timer)
         timerText.text = getString(R.string.timer_text, minutesLeftString, secondsLeftString)
 
     }
 
-
-//    fun updateCountDownTimer() {
-//        var timerText = findViewById<TextView>(R.id.countdown_Timer)
-//        val minutesUntilFinished = secondsRemaining / 60
-//        val secondsInMinuteUntilFinished = secondsRemaining - minutesUntilFinished * 60
-//        var secondsStr = secondsInMinuteUntilFinished.toString()
-//        if (secondsStr.length < 2) {
-//            secondsStr = "0" + secondsStr
-//        }
-//        timerText.text = "$minutesUntilFinished:$secondsStr"
-//    }
-
-    fun updatePushUpCounter(count : Int) {
+    fun updatePushUpCounter(count: Int) {
         var countText = findViewById<TextView>(R.id.pushup_counter)
         countText.text = getString(R.string.count_text, count)
-
     }
 
     override fun onBackPressed() {
